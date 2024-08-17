c9a09e53be0f5498e69c926c182954376e39708f
Eric Gaupp <eric.gaupp@gmail.com>
2024-08-17 18:28:51 +0100

    Added prettier and lint-staged; formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,59 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "../amplify/data/resource";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { generateClient } from 'aws-amplify/data'
+import type { Schema } from '../amplify/data/resource'
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
 
-Amplify.configure(outputs);
+Amplify.configure(outputs)
 
-const client = generateClient<Schema>();
+const client = generateClient<Schema>()
 
 const AddToDoButton = () => {
-  const handleCreateTodoClick = () => {
-    const content = window.prompt("Todo content");
-    if (content) {
-      client.models.Todo.create({ content })
-        .then((response) => {
-          if (!response.data || response.errors) {
-            console.error("Error creating todo:", response.errors);
-            alert("Failed to create todo.");
-          }
-        })
-        .catch((error) => {
-          console.error("Network or other error:", error);
-          alert("Failed to create todo due to a network or other error.");
-        });
+    const handleCreateTodoClick = () => {
+        const content = window.prompt('Todo content')
+        if (content) {
+            client.models.Todo.create({ content })
+                .then((response) => {
+                    if (!response.data || response.errors) {
+                        console.error('Error creating todo:', response.errors)
+                        alert('Failed to create todo.')
+                    }
+                })
+                .catch((error) => {
+                    console.error('Network or other error:', error)
+                    alert(
+                        'Failed to create todo due to a network or other error.'
+                    )
+                })
+        }
     }
-  };
-  return <button onClick={handleCreateTodoClick}>Add Todo</button>;
-};
+    return <button onClick={handleCreateTodoClick}>Add Todo</button>
+}
 
 const App = () => {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    const [todos, setTodos] = useState<Array<Schema['Todo']['type']>>([])
 
-  useEffect(() => {
-    const subscription = client.models.Todo.observeQuery().subscribe({
-      next: (data) => {
-        setTodos([...data.items]);
-      },
-    });
+    useEffect(() => {
+        const subscription = client.models.Todo.observeQuery().subscribe({
+            next: (data) => {
+                setTodos([...data.items])
+            },
+        })
 
-    return () => subscription.unsubscribe();
-  }, []);
+        return () => subscription.unsubscribe()
+    }, [])
 
-  return (
-    <main>
-      <ul>
-        {todos.map(({ content, id }) => (
-          <li key={id}>{content}</li>
-        ))}
-      </ul>
-      <AddToDoButton />
-    </main>
-  );
-};
+    return (
+        <main>
+            <ul>
+                {todos.map(({ content, id }) => (
+                    <li key={id}>{content}</li>
+                ))}
+            </ul>
+            <AddToDoButton />
+        </main>
+    )
+}
 
-export default App;
+export default App
